e8f32b98435b932768ba8bd6a3cdc373e5e25f3d
Mahmoud Emadeldin <mahmoudemad21497@gmail.com>
2026-01-10 13:45:56 +0200

    add notes state

--- lib/widgets/addItem.dart
+++ lib/widgets/addItem.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:store_app/consts.dart';
-import 'package:store_app/widgets/custom_Button.dart';
-import 'package:store_app/widgets/textField.dart';
+import 'package:store_app/widgets/addNote_form.dart';
 
 class addItem extends StatelessWidget {
   const addItem({
@@ -36,59 +35,5 @@ class addItem extends StatelessWidget {
         child: Icon(
           Icons.add,
         ));
-  }
-}
-
-class addNoteForm extends StatefulWidget {
-  addNoteForm({
-    super.key,
-  });
-
-  @override
-  State<addNoteForm> createState() => _addNoteFormState();
-}
-
-class _addNoteFormState extends State<addNoteForm> {
-  GlobalKey<FormState> key = GlobalKey();
-
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-
-  String? title, description;
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      autovalidateMode: autovalidateMode,
-      key: key,
-      child: Column(
-        children: [
-          textField(
-            hintText: 'Title',
-            onSave: (data) {
-              title = data;
-            },
-          ),
-          textField(
-            hintText: 'Description',
-            maxLines: 10,
-            onSave: (data) {
-              description = data;
-            },
-          ),
-          customButton(
-            text: 'Add Note',
-            onTap: () {
-              if (key.currentState!.validate()) {
-                key.currentState!.save();
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-              }
-              setState(() {});
-              // Navigator.pop(context);
-            },
-          )
-        ],
-      ),
-    );
   }
 }
