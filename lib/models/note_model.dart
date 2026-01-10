882b064df0b779cfb16f6781bc6fb9492427f2d4
Mahmoud Emadeldin <mahmoudemad21497@gmail.com>
2026-01-10 12:50:11 +0200

    create hive for project

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,10 +1,17 @@
-import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
-class NoteModel {
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String desc;
-  final DateTime date;
-  final Color color;
+  @HiveField(2)
+  final String date;
+  @HiveField(3)
+  final int color;
 
   NoteModel(
       {required this.title,
